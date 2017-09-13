6203a1b5cb7d3bd1abfd782fde85d69cdfde8357
Andrew Hamilton <ahamilton@katch.com>
2017-09-13 14:16:46 -0700

    Adds nginx log stats creator tool

---
+++ main.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/url"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const (
+	defaultLogInput  = "/var/log/nginx/access.log"
+	defaultLogOutput = "/var/log/stats.log"
+	defaultTimeout   = 5
+)
+
+var (
+	// reqStatusRegexp is used to pull out the request string and status code from each log line
+	// Though regexp aren't always the most performant, it's much easier here to grab the small amount of info we want than with splitting strings
+	reqStatusRegexp = `"([A-Z]+ .*)" ([2-5][0-9]+)`
+
+	logInput  string
+	logOutput string
+	seconds   int64
+)
+
+func init() {
+	// Flags to make it easier to test out the program
+	// Defaults are set here too
+	flag.StringVar(&logInput, "input", defaultLogInput, "input file used to get log entries")
+	flag.StringVar(&logOutput, "output", defaultLogOutput, "output file used to collect stats")
+	flag.Int64Var(&seconds, "timeout", defaultTimeout, "timeout in seconds to wait between collections")
+}
+
+func main() {
+	re := regexp.MustCompile(reqStatusRegexp)
+	flag.Parse()
+
+	timeout := defaultTimeout * time.Second
+
+	input := openFile(logInput)
+	defer input.Close()
+
+	output := openFile(logOutput)
+	defer output.Close()
+
+	reader := bufio.NewReader(input)
+
+	for {
+		statuses, badRoutes := stats(re, reader)
+
+		// Specify statuses here since if we don't read any new long lines we, the map will not have any fields
+		for _, level := range []string{"50x", "40x", "30x", "20x"} {
+			fmt.Fprintf(output, "%s:%d|s\n", level, statuses[level])
+		}
+
+		for route, hits := range badRoutes {
+			fmt.Fprintf(output, "%s:%d|s\n", route, hits)
+		}
+
+		wait := time.After(timeout)
+		<-wait
+	}
+}
+
+func openFile(filename string) *os.File {
+	f, err := os.Open(filename)
+	for err != nil {
+		if err != nil {
+			log.Printf("error opening log file: %v\n", err)
+		}
+
+		fileWait := time.After(2 * time.Second)
+		<-fileWait
+
+		f, err = os.Open(filename)
+	}
+
+	return f
+}
+
+// stats will read the file and pick out information using the provided regular expression
+func stats(re *regexp.Regexp, reader *bufio.Reader) (map[string]int, map[string]int) {
+	var readErr error
+	var byteLine []byte
+	s := make(map[string]int)
+	r := make(map[string]int)
+
+	byteLine, readErr = reader.ReadBytes('\n')
+
+	// Read the file until we reach the end
+	for readErr != io.EOF {
+		line := re.FindStringSubmatch(string(byteLine))
+
+		if len(line) >= 2 {
+			code, route, err := getStatus(line)
+			if err != nil {
+				continue
+			}
+
+			s[code]++
+
+			if route != "" {
+				r[route]++
+			}
+		}
+
+		byteLine, readErr = reader.ReadBytes('\n')
+	}
+
+	return s, r
+}
+
+// getStatus will find the status and route (if required) from a provided line
+func getStatus(line []string) (string, string, error) {
+	var s string
+	var r string
+
+	routeStatus := strings.Split(line[0], " ")
+	status, err := strconv.Atoi(line[2])
+	if err != nil {
+		return s, r, err
+	}
+
+	switch {
+	case status >= 200 && status < 300:
+		s = "20x"
+	case status >= 300 && status < 400:
+		s = "30x"
+	case status >= 400 && status < 500:
+		s = "40x"
+	case status >= 500:
+		s = "50x"
+
+		rawRoute, err := url.Parse(routeStatus[1])
+		if err != nil {
+			return "", "", err
+		}
+		r = rawRoute.Path
+	}
+
+	return s, r, nil
+}
---
+++ main_test.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"bufio"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+func TestGetStatus(t *testing.T) {
+	tests := []struct {
+		name           string
+		line           []string
+		expectedStatus string
+		expectedRoute  string
+	}{
+		{
+			name:           "20x",
+			line:           []string{`"GET /our-products/shave?action=redeem-gift-card HTTP/1.1" 200`, "GET /our-products/shave?action=redeem-gift-card HTTP/1.1", "200"},
+			expectedStatus: "20x",
+			expectedRoute:  "",
+		},
+		{
+			name:           "30x",
+			line:           []string{`"POST /our-products/shave?action=redeem-gift-card HTTP/1.1" 301`, "POST /our-products/shave?action=redeem-gift-card HTTP/1.1", "301"},
+			expectedStatus: "30x",
+			expectedRoute:  "",
+		},
+		{
+			name:           "40x",
+			line:           []string{`"GET /our-products/shave?action=redeem-gift-card HTTP/1.1" 403`, "GET /our-products/shave?action=redeem-gift-card HTTP/1.1", "403"},
+			expectedStatus: "40x",
+			expectedRoute:  "",
+		},
+		{
+			name:           "50x",
+			line:           []string{`"GET /our-products/shave?action=redeem-gift-card HTTP/1.1" 503`, "GET /our-products/shave?action=redeem-gift-card HTTP/1.1", "503"},
+			expectedStatus: "50x",
+			expectedRoute:  "/our-products/shave",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			s, r, err := getStatus(test.line)
+			if err != nil {
+				t.Errorf("unexpected error for test %s: %v", test.name, err)
+			}
+			if s != test.expectedStatus {
+				t.Errorf("unexpected status: expected %s, got %s", test.expectedStatus, s)
+			}
+			if r != test.expectedRoute {
+				t.Errorf("unexpected route: expected %s, got %s", test.expectedRoute, r)
+			}
+		})
+	}
+}
+
+func BenchmarkGetStatus(b *testing.B) {
+	line := []string{`"GET /our-products/shave?action=redeem-gift-card HTTP/1.1" 503`, "GET /our-products/shave?action=redeem-gift-card HTTP/1.1", "503"}
+	for i := 0; i < b.N; i++ {
+		getStatus(line)
+	}
+}
+
+func TestStats(t *testing.T) {
+	log := `10.10.180.161 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:14 +0000]  https https https "GET /our-products HTTP/1.1" 200 35967 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+50.112.166.232 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:14 +0000]  http https https "GET /our-products HTTP/1.0" 404 52176 "-" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+50.112.166.232 - 50.112.166.232, 192.33.28.238, 50.112.166.232,127.0.0.1 - - - [02/Aug/2015:15:56:14 +0000]  http https,http https,http "GET /api/v1/user HTTP/1.1" 200 3350 "https://release.dollarshaveclub.com/our-products" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+10.10.180.161 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:14 +0000]  https https https "GET /api/v1/user HTTP/1.1" 200 3350 "https://release.dollarshaveclub.com/our-products" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+50.112.166.232 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:14 +0000]  http https https "GET /api/v1/user HTTP/1.0" 200 3350 "https://release.dollarshaveclub.com/our-products" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+50.112.166.232 - 50.112.166.232, 192.33.28.238, 50.112.166.232,127.0.0.1 - - - [02/Aug/2015:15:56:27 +0000]  http https,http https,http "POST /api/v1/subscriptions/build HTTP/1.1" 200 6058 "https://release.dollarshaveclub.com/blades" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+10.10.180.40 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:27 +0000]  https https https "POST /api/v1/subscriptions/build HTTP/1.1" 503 6058 "https://release.dollarshaveclub.com/blades" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+50.112.166.232 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:27 +0000]  http https https "POST /api/v1/subscriptions/build HTTP/1.0" 200 6058 "https://release.dollarshaveclub.com/blades" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+10.10.180.161 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:28 +0000]  https https https "POST /api/v1/subscriptions/build HTTP/1.1" 301 6735 "https://release.dollarshaveclub.com/blades" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"
+50.112.166.232 - 50.112.166.232, 192.33.28.238 - - - [02/Aug/2015:15:56:28 +0000]  http https https "POST /api/v1/subscriptions/build HTTP/1.0" 200 6735 "https://release.dollarshaveclub.com/blades" "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.81 Safari/537.36"`
+
+	expectedStats := []struct {
+		lvl   string
+		value int
+	}{
+		{
+			lvl:   "20x",
+			value: 6,
+		},
+		{
+			lvl:   "30x",
+			value: 1,
+		},
+		{
+			lvl:   "40x",
+			value: 1,
+		},
+		{
+			lvl:   "50x",
+			value: 1,
+		},
+	}
+
+	expectedRoutes := []struct {
+		path  string
+		value int
+	}{
+		{
+			path:  "/api/v1/subscriptions/build",
+			value: 1,
+		},
+	}
+
+	re := regexp.MustCompile(reqStatusRegexp)
+	reader := bufio.NewReader(strings.NewReader(log))
+
+	s, r := stats(re, reader)
+
+	for _, stat := range expectedStats {
+		if s[stat.lvl] != stat.value {
+			t.Errorf("unexpected number of requests for status level %s: expected %d, got %d\n", stat.lvl, stat.value, s[stat.lvl])
+		}
+	}
+
+	for _, route := range expectedRoutes {
+		if _, ok := r[route.path]; !ok {
+			t.Errorf("missing path in returned routes: expected %s", route.path)
+			continue
+		}
+		if r[route.path] != route.value {
+			t.Errorf("unexpected number of requests for route %s: expected %d, got %d\n", route.path, route.value, r[route.path])
+		}
+	}
+}
